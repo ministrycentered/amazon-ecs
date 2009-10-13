ada659cf075bec75bf5599728c97b7e4ed38b41b
Jeffrey Berg <jsberg@Jeffrey-Bergs-MacBook-Pro.local>
2009-10-12 19:36:20 -0700

    Fixes Bug In New Rails Class Auto Loading

--- lib/amazon/aws_product_sign.rb
+++ lib/amazon/aws_product_sign.rb
@@ -47,101 +47,103 @@
 # key twice. I don't think the AWS service ever uses that though?
 #
 # This class also assumes a GET request. 
-class AwsProductSign  
+module Amazon
+  class AwsProductSign  
   
-  def initialize(options = {})
-    @secret_key = options[:secret_key]
-    raise Exception.new("You must supply a :secret_key") unless @secret_key
-    @access_key = options[:access_key]
-  end
+    def initialize(options = {})
+      @secret_key = options[:secret_key]
+      raise Exception.new("You must supply a :secret_key") unless @secret_key
+      @access_key = options[:access_key]
+    end
 
-  def query_with_signature(hash)
-    return hash_to_query( add_signature(hash)  )
-  end
+    def query_with_signature(hash)
+      return hash_to_query( add_signature(hash)  )
+    end
   
-  # Pass in a hash representing params for a query string.  
-  # param keys should be strings, not symbols please.
-  # Will return a param with the "Signature" key/value added, without
-  # modifying original. 
-  def add_signature(params)
-    # Make a copy to not modify original  
-    add_signature!( Hash[params]  )
-  end
+    # Pass in a hash representing params for a query string.  
+    # param keys should be strings, not symbols please.
+    # Will return a param with the "Signature" key/value added, without
+    # modifying original. 
+    def add_signature(params)
+      # Make a copy to not modify original  
+      add_signature!( Hash[params]  )
+    end
   
-  # Like #add_signature, but will mutate the hash passed in, 
-  # adding a "Signature" key/value to hash passed in, and return
-  # hash too.  
-  def add_signature!(params)
+    # Like #add_signature, but will mutate the hash passed in, 
+    # adding a "Signature" key/value to hash passed in, and return
+    # hash too.  
+    def add_signature!(params)
     
-    # supply timestamp and access key if not already provided
-    params["Timestamp"] ||= Time.now.iso8601
-    params["AWSAccessKeyId"] ||= access_key
-    # Existing "Signature"? That's gotta go before we generate a new
-    # signature and add it. 
-    params.delete("Signature")
+      # supply timestamp and access key if not already provided
+      params["Timestamp"] ||= Time.now.iso8601
+      params["AWSAccessKeyId"] ||= access_key
+      # Existing "Signature"? That's gotta go before we generate a new
+      # signature and add it. 
+      params.delete("Signature")
 
-    query_string = canonical_querystring(params)
+      query_string = canonical_querystring(params)
 
-    string_to_sign = string_to_sign(query_string)
+      string_to_sign = string_to_sign(query_string)
 
-    hmac = HMAC::SHA256.new( secret_key )
-    hmac.update( string_to_sign )
-    # chomp is important!  the base64 encoded version will have a newline at the end
-    signature = Base64.encode64(hmac.digest).chomp 
+      hmac = HMAC::SHA256.new( secret_key )
+      hmac.update( string_to_sign )
+      # chomp is important!  the base64 encoded version will have a newline at the end
+      signature = Base64.encode64(hmac.digest).chomp 
 
-    params["Signature"] = signature
+      params["Signature"] = signature
 
-    #order doesn't matter for the actual request, we return the hash
-    #and let client turn it into a url.
-    return params
-  end
+      #order doesn't matter for the actual request, we return the hash
+      #and let client turn it into a url.
+      return params
+    end
 
-  # Insist on specific method of URL encoding, RFC3986. 
-  def url_encode(string)
-    # It's kinda like CGI.escape, except CGI.escape is encoding a tilde when
-    # it ought not to be, so we turn it back. Also space NEEDS to be %20 not +.
-    return CGI.escape(string).gsub("%7E", "~").gsub("+", "%20")
-  end
+    # Insist on specific method of URL encoding, RFC3986. 
+    def url_encode(string)
+      # It's kinda like CGI.escape, except CGI.escape is encoding a tilde when
+      # it ought not to be, so we turn it back. Also space NEEDS to be %20 not +.
+      return CGI.escape(string).gsub("%7E", "~").gsub("+", "%20")
+    end
 
-  # param keys should be strings, not symbols please. return a string joined
-  # by & in canonical order. 
-  def canonical_querystring(params)
-    # I hope this built-in sort sorts by byte order, that's what's required. 
-    values = params.keys.sort.collect {|key|  [url_encode(key), url_encode(params[key].to_s)].join("=") }
+    # param keys should be strings, not symbols please. return a string joined
+    # by & in canonical order. 
+    def canonical_querystring(params)
+      # I hope this built-in sort sorts by byte order, that's what's required. 
+      values = params.keys.sort.collect {|key|  [url_encode(key), url_encode(params[key].to_s)].join("=") }
     
-    return values.join("&")
-  end
+      return values.join("&")
+    end
 
-  def string_to_sign(query_string, options = {})
-    options[:verb] = "GET"
-    options[:request_uri] = "/onca/xml"
-    options[:host] = "webservices.amazon.com"
+    def string_to_sign(query_string, options = {})
+      options[:verb] = "GET"
+      options[:request_uri] = "/onca/xml"
+      options[:host] = "webservices.amazon.com"
 
     
-    return options[:verb] + "\n" + 
-        options[:host].downcase + "\n" +
-        options[:request_uri] + "\n" +
-        query_string
-  end
+      return options[:verb] + "\n" + 
+          options[:host].downcase + "\n" +
+          options[:request_uri] + "\n" +
+          query_string
+    end
 
-  # Turns a hash into a query string, returns the query string.
-  # url-encodes everything to Amazon's specifications. 
-  def hash_to_query(hash)
-    hash.collect do |key, value|
+    # Turns a hash into a query string, returns the query string.
+    # url-encodes everything to Amazon's specifications. 
+    def hash_to_query(hash)
+      hash.collect do |key, value|
       
-      url_encode(key) + "=" + url_encode(value)
+        url_encode(key) + "=" + url_encode(value)
     
-    end.join("&")
-  end
+      end.join("&")
+    end
 
-  def secret_key
-    return @secret_key
-  end
-  def access_key
-    return @access_key
-  end
-  def access_key=(a)
-    @access_key = a
-  end
+    def secret_key
+      return @secret_key
+    end
+    def access_key
+      return @access_key
+    end
+    def access_key=(a)
+      @access_key = a
+    end
   
+  end
 end
